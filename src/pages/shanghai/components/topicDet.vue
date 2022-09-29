f604ed3a8ff66c0df49eace6314ecff8551b88cf
桥非路 <913960650@qq.com>
2022-09-29 17:48:29 +0800

    图表基本确定，除了图谱

--- src/pages/shanghai/components/topicDet.vue
+++ src/pages/shanghai/components/topicDet.vue
@@ -29,7 +29,9 @@
     </a-col>
     <a-col :sm="24" :md="24" :xl="16" :style="{ marginBottom: '24px' }">
       <a-card title="话题下发博人地理信息地图展示" :bordered="false">
-        <p>地图实例</p>
+<!--        <p>地图实例</p>-->
+        <div id="map-container" style="min-height: 500px; justify-content: center;position: relative">
+        </div>
       </a-card>
     </a-col>
     <a-col :sm="24" :md="24" :xl="24" :style="{ marginBottom: '24px' }">
@@ -54,6 +56,7 @@ for (let i = 0; i < 23; i++) {
         '上海市疫情防控核酸筛查专班负责人夏科家表示，对于“常态化检测点不接受核酸报告超过72小时”的说法，市防控办没有出台过此类要求，我们已要求各区开展自查，如有个别采样点擅自加码，必须坚决整改。另外需要说明一下，大家出入居住的本社区，不需要提供72小时内核酸阴性证明。但是需要规范佩戴口罩，配合测温，并主动扫出入口的“场所码”，出示“随申码”绿码'
   })
 }
+import { Dot} from '@antv/l7plot'
 import {mapState} from 'vuex'
 export default {
   name: 'TopicDetail',
@@ -73,8 +76,58 @@ export default {
       ],
     }
   },
+  methods:{
+    mapCreate(){
+      fetch('https://gw.alipayobjects.com/os/antfincdn/xZqmXatMnc/quanguojiaotongshijianxiangyingzhishu.json')
+          .then((response) => response.json())
+          .then((data) => {
+            new Dot('map-container', {
+              map: {
+                type: 'amap',
+                style: 'dark',
+                center: [102.447303, 37.753574],
+                zoom: 2,
+                pitch: 0,
+              },
+              source: {
+                data: data,
+                parser: {
+                  type: 'geojson',
+                },
+              },
+              color: '#4cfd47',
+              size: 20,
+              animate: true,
+              state: { active: true },
+              autoFit: true,
+              label: {
+                field: 'cityName',
+                style: {
+                  fill: '#fff',
+                  fontSize: 12,
+                  textAnchor: 'top',
+                  textOffset: [0, 20],
+                },
+              },
+              zoom: {
+                position: 'bottomright',
+              },
+              layerMenu: {
+                position: 'topright',
+              },
+              tooltip: {
+                items: [{ field: 'cityName', alias: '名称' }],
+              },
+            });
+          });
+    }
+
+  },
   computed: {
     ...mapState('setting', ['pageMinHeight']),
+  },
+  mounted() {
+    this.mapCreate()
   }
 }
 </script>
--- src/pages/shanghai/components/weiboDet.vue
+++ src/pages/shanghai/components/weiboDet.vue
@@ -8,13 +8,14 @@
       </a-col>
       <a-col :sm="24" :md="24" :xl="16" :style="{ marginBottom: '24px' }">
         <a-card title="博文评论主题类型" :bordered="false">
-          <p>评论类型</p>
+<!--          <p>评论类型</p>-->
           <div id="mountNode"></div>
         </a-card>
       </a-col>
       <a-col :sm="24" :md="24" :xl="24" :style="{ marginBottom: '24px' }">
         <a-card title="转发地图" :bordered="false">
-          <p>转发地图展示区域</p>
+<!--          <p>转发地图展示区域</p>-->
+          <div id="map123" style="min-height: 500px; justify-content: center;position: relative"></div>
         </a-card>
       </a-col>
       <a-col :sm="24" :md="24" :xl="24" :style="{ marginBottom: '24px' }">
@@ -29,6 +30,7 @@
 <script>
 import {mapState} from 'vuex'
 import { Pie } from '@antv/g2plot';
+import { Flow } from '@antv/l7plot';
 export default {
   name: 'TopicDetail',
   data() {
@@ -45,7 +47,6 @@ export default {
   },
   methods:{
     mountNode(){
-      console.log()
       const piePlot = new Pie('mountNode', {
         appendPadding: 10,
         data:this.pieData,
@@ -59,13 +60,157 @@ export default {
         interactions: [{ type: 'pie-legend-active' }, { type: 'element-active' }],
       });
       piePlot.render();
-    }
+    },
+    forwardMap(){
+      // fetch('https://gw.alipayobjects.com/os/antfincdn/SIybYh6xr1/arc.json')
+      //     .then((response) => response.json())
+      //     .then((data) => {
+      //       new Flow('map', {
+      //         map: {
+      //           type: 'amap',
+      //           style: 'dark',
+      //           center: [116.3956, 39.9392],
+      //           pitch: 45,
+      //           zoom: 10,
+      //         },
+      //         source: {
+      //           data: data,
+      //           parser: {
+      //             type: 'json',
+      //             x: 'x1',
+      //             y: 'y1',
+      //             x1: 'x',
+      //             y1: 'y',
+      //           },
+      //         },
+      //         autoFit: true,
+      //         shape: 'arc3d',
+      //         size: 1.5,
+      //         color: {
+      //           field: 'count',
+      //           value: ['rgba(1,124,247,0.9)', 'rgba(230,129,28,0.9)'],
+      //           scale: { type: 'quantize' },
+      //         },
+      //         style: {
+      //           opacity: 0.8,
+      //           segmentNumber: 60,
+      //         },
+      //         animate: {
+      //           interval: 2,
+      //           trailLength: 1,
+      //           duration: 2,
+      //         },
+      //         radiation: {
+      //           color: 'white',
+      //           size: 30,
+      //         },
+      //         label: {
+      //           visible: true,
+      //           field: 'count',
+      //           style: {
+      //             fill: '#000',
+      //             opacity: 0.8,
+      //             fontSize: 10,
+      //             stroke: '#fff',
+      //             strokeWidth: 2,
+      //             textAllowOverlap: false,
+      //             padding: [5, 5],
+      //             textOffset: [0, 35],
+      //           },
+      //         },
+      //         zoom: {
+      //           position: 'bottomright',
+      //         },
+      //         scale: {
+      //           position: 'bottomright',
+      //         },
+      //         layerMenu: {
+      //           position: 'topright',
+      //         },
+      //         legend: {
+      //           position: 'bottomleft',
+      //         },
+      //       });
+      //     });
+      fetch('https://gw.alipayobjects.com/os/antfincdn/SIybYh6xr1/arc.json')
+          .then((response) => response.json())
+          .then((data) => {
+            new Flow('map123', {
+              map: {
+                type: 'amap',
+                style: 'dark',
+                center: [116.3956, 39.9392],
+                pitch: 0,
+                zoom: 10,
+              },
+              source: {
+                data: data,
+                parser: {
+                  type: 'json',
+                  x: 'x1',
+                  y: 'y1',
+                  x1: 'x',
+                  y1: 'y',
+                },
+              },
+              autoFit: true,
+              shape: 'arc',
+              size: 1.5,
+              color: {
+                field: 'count',
+                value: ['rgba(1,124,247,0.9)', 'rgba(230,129,28,0.9)'],
+                scale: { type: 'quantize' },
+              },
+              style: {
+                opacity: 0.8,
+                segmentNumber: 60,
+              },
+              animate: {
+                interval: 2,
+                trailLength: 1,
+                duration: 2,
+              },
+              radiation: {
+                color: 'white',
+                size: 30,
+              },
+              label: {
+                visible: true,
+                field: 'count',
+                style: {
+                  fill: '#000',
+                  opacity: 0.8,
+                  fontSize: 10,
+                  stroke: '#fff',
+                  strokeWidth: 2,
+                  textAllowOverlap: false,
+                  padding: [5, 5],
+                  textOffset: [0, 35],
+                },
+              },
+              zoom: {
+                position: 'bottomright',
+              },
+              scale: {
+                position: 'bottomright',
+              },
+              layerMenu: {
+                position: 'topright',
+              },
+              legend: {
+                position: 'bottomleft',
+              },
+            });
+          });
+    },
   },
   computed: {
     ...mapState('setting', ['pageMinHeight']),
   },
   mounted() {
-    this.mountNode(this.pieData)
+    this.forwardMap()
+    this.mountNode()
+
   }
 }
 </script>
