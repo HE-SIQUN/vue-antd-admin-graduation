d6dd36795fa204a879ffc64b824ea4b0af422fb9
桥非路 <913960650@qq.com>
2022-10-12 21:15:14 +0800

    事件下添加了对应的地图

--- src/pages/shanghai/components/keyWord.vue
+++ src/pages/shanghai/components/keyWord.vue
@@ -2,7 +2,7 @@
   <div>
       <a-row :gutter="24" >
         <a-col :sm="24" :md="24" :xl="12" :style="{ marginBottom: '24px'}">
-          <a-card title="关键词话题博文热度列表" :bordered="false" :style="{height:'500px'}">
+          <a-card title="事假中话题博文热度列表" :bordered="false" :style="{height:'500px'}">
 <!--            v-for="(item,index) in listDataForm" :key="index"-->
               <div class="list-item-keyword" v-for="(item,index) in listDataForm" :key="index">
                 <router-link to="/incident/shanghai/detail" :style="{fontsize:'15px'}" >
@@ -24,17 +24,29 @@
             <div id="chart-cloud"></div>
           </a-card>
         </a-col>
-        <a-col :sm="24" :md="24" :xl="16" :style="{ marginBottom: '24px' }">
-          <a-card title="话题时间热度" :bordered="false">
-            <div id="line-container"></div>
-          </a-card>
-        </a-col>
-        <a-col :sm="24" :md="24" :xl="8" :style="{ marginBottom: '24px' }">
-          <a-card title="话题用户评析" :bordered="false">
-            <p>card content</p>
-          </a-card>
-        </a-col>
       </a-row>
+    <a-row :gutter="24" >
+      <a-col :sm="24" :md="24" :xl="24" :style="{ marginBottom: '24px' }">
+        <a-card title="话题时间热度" :bordered="false">
+          <div id="line-container"></div>
+        </a-card>
+      </a-col>
+<!--      <a-col :sm="24" :md="24" :xl="8" :style="{ marginBottom: '24px' }">-->
+<!--        <a-card title="话题用户评析" :bordered="false">-->
+<!--          <p>card content</p>-->
+<!--        </a-card>-->
+<!--      </a-col>-->
+    </a-row>
+    <a-row :gutter="24">
+      <a-col :sm="24" :md="24" :xl="24" :style="{ marginBottom: '24px'}">
+        <a-card title="事件分布热度" :bordered="false">
+          <div id="space-container" style="min-height: 550px">
+          </div>
+        </a-card>
+
+      </a-col>
+
+    </a-row>
   </div>
 </template>
 
@@ -53,6 +65,7 @@ for (let i = 0; i < 23; i++) {
 }
 import {mapState} from 'vuex'
 import { Line ,WordCloud} from '@antv/g2plot';
+import { Dot } from '@antv/l7plot';
 export default {
   name: 'TopicDetail',
   data() {
@@ -125,6 +138,62 @@ export default {
       // if(page===1){
       //
       // }
+    },
+    spaceMap(){
+      fetch('https://gw.alipayobjects.com/os/antfincdn/UvXSmhbwQx/zhongguochengshirenkoushuliangjiGDPpaihang.json')
+          .then((response) => response.json())
+          .then((data) => {
+            const colors = [
+              'rgba(254,255,198,0.95)',
+              'rgba(255,238,149,0.95)',
+              'rgba(255,217,99,0.95)',
+              'rgba(255,175,43,0.95)',
+              'rgba(255,135,24,0.95)',
+              'rgba(234,10,0,0.95)',
+              'rgba(195,0,0,0.95)',
+              'rgba(139,0,0,0.95)',
+            ];
+
+            new Dot('space-container', {
+              map: {
+                type: 'amap',
+                style: 'dark',
+                center: [102.601, 37.32],
+                zoom: 3,
+                pitch: 0,
+              },
+              source: {
+                data: data,
+                parser: { type: 'geojson' },
+              },
+              color: {
+                field: 'PerCapitaGDP',
+                value: ({ PerCapitaGDP }) => {
+                  const index = Math.min(7, ~~(PerCapitaGDP / 10000));
+                  return colors[index];
+                },
+              },
+              size: {
+                field: 'population',
+                value: ({ population }) => population / 80,
+              },
+              style: {
+                opacity: 1,
+                strokeWidth: 0,
+              },
+              state: { active: { color: '#1EA7FD' } },
+              tooltip: {
+                items: [
+                  { field: '名称', alias: '名称' },
+                  { field: 'PerCapitaGDP', alias: '人均GDP' },
+                  { field: 'population', alias: '人口' },
+                ],
+              },
+              zoom: {
+                position: 'bottomright',
+              },
+            });
+          });
     }
   },
   computed: {
@@ -134,6 +203,7 @@ export default {
   mounted() {
     this.test()
     this.cloud()
+    this.spaceMap()
   }
 }
 </script>
